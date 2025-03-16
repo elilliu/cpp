029ef4ac21be60c6209650dccd931e1a41d6004c
Emma Lilliu <emma.lilliu@gmail.com>
2025-03-16 22:48:53 +0100

    working on ex02

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,13 +1,71 @@
 #include "Account.hpp"
 
-Account::Account()
-{
-
-}
+int	Account::_nbAccounts;
+int	Account::_totalAmount;
+int	Account::_totalNbDeposits;
+int	Account::_totalNbWithdrawals;
 
 Account::Account(int initial_deposit)
 {
 
 }
 
+Account::~Account()
+{
 
+}
+
+Account::Account()
+{
+
+}
+
+int	Account::getNbAccounts()
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount()
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits()
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals()
+{
+	return (_totalNbWithdrawals);
+}
+
+void    Account::displayAccountsInfos()
+{
+
+}
+
+void	Account::makeDeposit(int deposit)
+{
+
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+
+}
+
+int	Account::checkAmount() const
+{
+
+}
+
+void	Account::displayStatus() const
+{
+
+}
+
+void	Account::_displayTimestamp()
+{
+
+}
