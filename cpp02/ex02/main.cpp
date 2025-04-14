7e6722112725d85a9a0328a4c5db55ab03830baa
Emma Lilliu <elilliu@paul-f5Br5s2.clusters.42paris.fr>
2025-04-14 13:14:12 +0200

    finished cpp02

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -5,15 +5,15 @@ int	main()
 	Fixed		a;
 	Fixed const	b(Fixed(5.05f) * Fixed(2));
 
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
+	std::cout << "a: " << a << " (" << a.getRawBits() << ")" << std::endl;
+	std::cout << "++a: " << ++a << " (" << a.getRawBits() << ")" << std::endl;
+	std::cout << "a: " << a << " (" << a.getRawBits() << ")" << std::endl;
+	std::cout << "a++: " << a++ << " (" << a.getRawBits() << ")" << std::endl;
+	std::cout << "a: " << a << " (" << a.getRawBits() << ")" << std::endl;
 
-	std::cout << b << std::endl;
+	std::cout << "b: " << b << " (" << b.getRawBits() << ")" << std::endl;
 
-	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << "maximum of a and b: " << Fixed::max(a, b) << std::endl;
 
 	return (0);
 }
