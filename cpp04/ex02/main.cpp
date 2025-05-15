548aacf0b44661c0f33b4c291477fe9db83f3124
Emma Lilliu <elilliu@f6r4s15.clusters.42paris.fr>
2025-05-15 16:25:01 +0200

    removd executables. Ready to push

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -4,11 +4,42 @@
 
 int	main()
 {
+	const Animal*	j = new Dog();
 	const Animal*	i = new Cat();
 
-	i->makeSound();
 
+	delete j;
 	delete i;
+
+	std::cout << '\n';
+
+	const Animal*	flock[10];
+
+	for (int i = 0; i < 5; i++)
+		flock[i] = new Dog();
+	for (int i = 5; i < 10; i++)
+		flock[i] = new Cat();
+
+	std::cout << '\n';
+
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << i << " is a " << flock[i]->getType() << std::endl;
+		flock[i]->makeSound();
+	}
+
+	std::cout << '\n';
+
+	for (int i = 0; i < 10; i++)
+		delete flock[i];
+
+	// std::cout << '\n';
+
+	// const Animal*	random = new Animal();
+
+	// random->makeSound();
+
+	// delete random;
 
 	return (0);
 }
